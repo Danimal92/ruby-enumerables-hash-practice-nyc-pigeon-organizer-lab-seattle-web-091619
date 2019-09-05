5ea2890f177a445eecbd72c354b23beb8e1b8a3d
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-09-05 09:05:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+ 
+  data.each{|attribute, attribute_hash|
+    attribute_hash.each{|key,name_array|
+      name_array.each{|name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {attribute => [key.to_s]}
+          
+        else 
+          
+          pigeon_list[name][attribute] << [key]
+          pigeon_list[name][attribute][key] = pigeon_list[name][attribute][key].flatten
+
+        end
+
+
+
+
+
+      }
+    }
+  }
+  return pigeon_list 
 end
+
